80871acafe37f5bda97de3302b01b059acba9dd5
AyeshaSajid2 <147182897+AyeshaSajid2@users.noreply.github.com>
2024-03-15 20:08:35 +0500

    Updated dua in ramdan dua screen

--- lib/main_screen/more_components/ramzan_dua.dart
+++ lib/main_screen/more_components/ramzan_dua.dart
@@ -2,100 +2,104 @@ import 'package:flutter/material.dart';
 import 'package:the_light_al_noor/global/colors.dart';
 
 class AshraDuaWidget extends StatelessWidget {
-  final String imagePath;
   final String duaText;
   final String duaTranslation;
   final String duaTranslationUrdu;
+  final String customText;
+  final String imagePath;
 
   const AshraDuaWidget({
     Key? key,
-    required this.imagePath,
     required this.duaText,
     required this.duaTranslation,
     required this.duaTranslationUrdu,
+    required this.customText,
+    required this.imagePath,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.all(10.0),
+      margin: const EdgeInsets.all(10.0),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(20.0),
         boxShadow: [
           BoxShadow(
-            color: Colors.black.withOpacity(0.2),
+            color: Colors.white.withOpacity(0.2),
             blurRadius: 5.0,
             spreadRadius: 2.0,
-            offset: Offset(0, 3),
+            offset: const Offset(0, 3),
           ),
         ],
       ),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisSize: MainAxisSize.min,
         children: [
-          ClipRRect(
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(20.0),
-              topRight: Radius.circular(20.0),
-            ),
-            child: Opacity(
-              opacity: 0.45,
-              child: Image.asset(
-                imagePath,
-                fit: BoxFit.cover,
-                height: 200,
+          Stack(
+            alignment: Alignment.bottomCenter,
+            children: [
+              Opacity(
+                opacity: 0.45,
+                child: Image.asset(
+                  imagePath,
+                  fit: BoxFit.cover,
+                  width: double.infinity,
+                  height: 200,
+                ),
               ),
-            ),
+              Container(
+                padding: const EdgeInsets.all(10.0),
+                color: Colors.black.withOpacity(0.2),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      duaText,
+                      textAlign: TextAlign.center,
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 5.0),
+                    Text(
+                      duaTranslation,
+                      textAlign: TextAlign.center,
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 16.0,
+                      ),
+                    ),
+                    const SizedBox(height: 5.0),
+                    Text(
+                      duaTranslationUrdu,
+                      textAlign: TextAlign.center,
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 16.0,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
           Container(
-            padding: EdgeInsets.all(10.0),
-            color: Colors.black.withOpacity(0.5),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text(
-                  duaText,
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20.0,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                SizedBox(height: 5.0),
-                Text(
-                  duaTranslation,
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 16.0,
-                  ),
-                ),
-                SizedBox(height: 5.0),
-                Text(
-                  duaTranslationUrdu,
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 16.0,
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.symmetric(vertical: 10.0),
-            color: Colors.black.withOpacity(0.5),
+            padding: const EdgeInsets.symmetric(vertical: 10.0),
+            color: primaryColor2,
             child: Text(
-              'روزہ رکھنے کی نیت',
+              customText,
               textAlign: TextAlign.center,
-              style: TextStyle(
+              style: const TextStyle(
                 color: Colors.white,
                 fontSize: 18.0,
                 fontWeight: FontWeight.bold,
               ),
             ),
           ),
+          SizedBox(height: 15,),
         ],
       ),
     );
@@ -103,46 +107,88 @@ class AshraDuaWidget extends StatelessWidget {
 }
 
 class FirstAshraDua extends StatelessWidget {
+  const FirstAshraDua({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: secondaryColor,
       appBar: AppBar(
         backgroundColor: primaryColor,
-        title: Text('Ashra Duas'),
+        title: const Text('Ashra Duas'),
       ),
       body: ListView(
-        children: [
+        children: const [
           AshraDuaWidget(
             imagePath: 'assets/images/ramadan_images/dua_phla_ashray.jpg',
-            duaText: 'وَبِصَوْمِ غَدٍ نَّوَيْتُ مِنْ شَهْرِ رَمَضَانََ',
-            duaTranslationUrdu: '’اورمیں نے ماہ رمضان کے کل کے روزے کی نیت کی',
-            duaTranslation: 'I Intend to keep the fast for month of Ramadan',
+            duaText: 'وَبِصَوْمِ غَدٍ نَّوَيْتُ مِنْ شَهْرِ رَمَضَانَ',
+            duaTranslation:
+            'I Intend to keep the fast for month of Ramadan',
+            duaTranslationUrdu:
+            'اورمیں نے ماہ رمضان کے کل کے روزے کی نیت کی.',
+            customText: 'روزہ رکھنے کی نیت',
           ),
           AshraDuaWidget(
-            imagePath: 'assets/images/ramadan_images/dua_phla_ashray.jpg',
-            duaText: 'رَبِّ اغْفِرْ وَارْحَمْ وَأَنْتَ خَيْرُ الرَّاحِمِينَ',
-            duaTranslation: 'Allah! Forgive me and have mercy on me. You are the Most Merciful of all',
-            duaTranslationUrdu: 'اللہ! مجھے بخش دے اور مجھ پر رحم فرما۔ آپ سب سے زیادہ رحم کرنے والے ہیں۔',
+            imagePath: 'assets/images/png/top.jpg',
+            duaText: 'اَللّٰهُمَّ اِنَّی لَکَ صُمْتُ وَبِکَ اٰمَنْتُ وَعَلَيْکَ تَوَکَّلْتُ وَعَلٰی رِزْقِکَ اَفْطَرْتُ.َ',
+            duaTranslationUrdu:
+            'اے اللہ!میں نے تیری خاطر روزہ رکھا اور تیرے اوپر ایمان لایا اور تجھ پر بھروسہ کیا اورتیرے رزق سے اسے کھول رہا ہوں۔',
+            duaTranslation:
+            'O Allah! I fasted for you and I believe in you and I put my trust in You and I break my fast with your sustenance.',
+            customText: 'روزہ افطار کرنے کی دعا',
           ),
+
           AshraDuaWidget(
-            imagePath: 'assets/images/ramadan_images/dua_phla_ashray.jpg',
-            duaText: 'رَبِّ اغْفِرْ وَارْحَمْ وَأَنْتَ خَيْرُ الرَّاحِمِينَ',
-            duaTranslation: 'Allah! Forgive me and have mercy on me. You are the Most Merciful of all',
-            duaTranslationUrdu: 'اللہ! مجھے بخش دے اور مجھ پر رحم فرما۔ آپ سب سے زیادہ رحم کرنے والے ہیں۔',
+            imagePath: 'assets/images/png/top.jpg',
+            duaText: 'ذَھَبَ الظَّمَأُوَابْتَلَّتِ الْعُرُوْقُ وَثَبَتَ الْاَجْرُ اِنْ شَآئَ اللّٰہُ۔َ',
+            duaTranslationUrdu:
+            'پیاس جاتی رہی اور رگیں تر ہوگئیں اور ثواب ثابت ہوگیا ان شاء اللہ تعالیٰ۔۔',
+            duaTranslation:
+            'Thirst has gone, the veins are moist, and the reward is assured, if Allah wills.',
+            customText: 'افطارکرنے کے بعد کی دعا',
           ),
+
           AshraDuaWidget(
-            imagePath: 'assets/images/ramadan_images/dua_phla_ashray.jpg',
-            duaText: 'رَبِّ اغْفِرْ وَارْحَمْ وَأَنْتَ خَيْرُ الرَّاحِمِينَ',
-            duaTranslation: 'Allah! Forgive me and have mercy on me. You are the Most Merciful of all',
-            duaTranslationUrdu: 'اللہ! مجھے بخش دے اور مجھ پر رحم فرما۔ آپ سب سے زیادہ رحم کرنے والے ہیں۔',
+            imagePath: 'assets/images/png/top.jpg',
+            duaText: 'اَفْطَرَعِنْدَ کُمُ الصَّآئِمُوْنَ وَاَکَلَ طَعَامَکُمُ الْابَرَارُ وَصَلَّتْ عَلَیْکُمُ الْمَلَآئِکَةُ ۔َ',
+            duaTranslationUrdu:
+            'افطارکیا کریں تمہارے یہاں روزہ دار لوگ اور کھایا کریں تمہارا کھانا نیک لوگ اور رحمت کی دعا کیا کریں تمہارے لئے فرشتے ۔',
+            duaTranslation:
+            'May the fasting (people) break their fast in your home, and may the dutiful and pious eat your food, and may the angels send prayers upon you.',
+            customText: 'کسی کے ہاں روزہ افطار کرے تو یہ دعاپڑھے',
           ),
+
           AshraDuaWidget(
-            imagePath: 'assets/images/ramadan_images/dua_phla_ashray.jpg',
-            duaText: 'رَبِّ اغْفِرْ وَارْحَمْ وَأَنْتَ خَيْرُ الرَّاحِمِينَ',
-            duaTranslation: 'Allah! Forgive me and have mercy on me. You are the Most Merciful of all',
-            duaTranslationUrdu: 'اللہ! مجھے بخش دے اور مجھ پر رحم فرما۔ آپ سب سے زیادہ رحم کرنے والے ہیں۔',
+            imagePath: 'assets/images/png/top.jpg',
+            duaText: 'يَا حَيُّ يَا قَيُّومُ بِرَحْمَتِكَ أَسْتَغيث',
+            duaTranslationUrdu:
+            'پیاس جاتی رہی اور رگیں تر ہوگئیں اور ثواب ثابت ہوگیا ان شاء اللہ تعالیٰ۔۔',
+            duaTranslation:
+            'Oh, Everliving, The Everlasting, I seek Your help through Your mercy.',
+            customText: 'افطارکرنے کے بعد کی دعا',
           ),
+
+          AshraDuaWidget(
+            imagePath: 'assets/images/png/top.jpg',
+            duaText: 'اَسْتَغْفِرُ اللہَ رَبِّی مِنْ کُلِّ زَنْبٍ وَّ اَتُوْبُ اِلَیْہِ',
+            duaTranslationUrdu:
+            'پیاس جاتی رہی اور رگیں تر ہوگئیں اور ثواب ثابت ہوگیا ان شاء اللہ تعالیٰ۔۔',
+            duaTranslation:
+            'I seek forgiveness from Allah for all my sins I committed and turn to Him.',
+            customText: 'افطارکرنے کے بعد کی دعا',
+          ),
+
+
+          AshraDuaWidget(
+            imagePath: 'assets/images/png/top.jpg',
+            duaText: 'اَللَّهُمَّ أَجِرْنِي مِنَ النَّارِ',
+            duaTranslationUrdu:
+            'پیاس جاتی رہی اور رگیں تر ہوگئیں اور ثواب ثابت ہوگیا ان شاء اللہ تعالیٰ۔۔',
+            duaTranslation:
+            'O Allah, save me from the fire.',
+            customText: 'افطارکرنے کے بعد کی دعا',
+          ),
+          // Add more AshraDuaWidget as needed
         ],
       ),
     );
